ef919ba4ee4af46733af607ca685536de23ce9ed
hadi <hadi.mohammad.saleh1@gmail.com>
2021-12-27 16:04:01 +0300

    d

---
+++ proj/hardhat.config.js
@@ -0,0 +1,29 @@
+require('@nomiclabs/hardhat-waffle')
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+module.exports = {
+  solidity: {
+    version: "0.7.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      url: "",
+      accounts: [
+        ""
+      ] // add your Ethereum account here (private key)
+    },
+  }
+};
